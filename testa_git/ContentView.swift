514506fb959bd2c5d58a55918c54781c4f5fbc15
jörgen persson <jorgen_raby@icloud.com>
2021-11-06 10:22:01 +0100

    Initial Commit

---
+++ testa_git/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testa_git
+//
+//  Created by jörgen persson on 2021-11-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ testa_git/testa_gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  testa_gitApp.swift
+//  testa_git
+//
+//  Created by jörgen persson on 2021-11-06.
+//
+
+import SwiftUI
+
+@main
+struct testa_gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
