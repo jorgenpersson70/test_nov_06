3a953fbd241035ce74c1b536fa2a14b2b370f8da
jörgen persson <jorgen_raby@icloud.com>
2021-11-06 13:00:53 +0100

    testa

--- testa_git/ContentView.swift
+++ testa_git/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world! hej världen hej")
+        Text("Hello, world! hej världen hej hej")
             .padding()
     }
 }
